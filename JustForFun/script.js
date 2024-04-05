192e483912f696e79ac27dce4f0ad3e1ce37c5de
Abhishek kumar <136875844+Abhisheksaw@users.noreply.github.com>
2024-04-05 21:59:38 +0530

    Add files via upload

---
+++ JustForFun/script.js
@@ -0,0 +1,21 @@
+const target = document.getElementById("target");
+let yes = document.getElementById("yes");
+
+yes.addEventListener("click", () => {
+  alert("Don't worry it's secret 🥰😊");
+});
+
+function moveTarget() {
+  const maxWidth = 400;
+  const maxHeight = 400;
+
+  const randomX = Math.floor(Math.random() * maxWidth);
+  const randomY = Math.floor(Math.random() * maxHeight);
+
+  target.style.left = randomX + "px";
+  target.style.top = randomY + "px";
+}
+
+target.addEventListener("mouseenter", function () {
+  moveTarget();
+});
